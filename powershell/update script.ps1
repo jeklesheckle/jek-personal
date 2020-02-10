a2a5fa073fd26e8f832744a1ce9701c87cfaf14b
Jackson Pielstick <jpielstick@salisbury.md>
2020-02-10 12:58:14 -0500

    minor change

--- powershell/update script.ps1
+++ powershell/update script.ps1
@@ -12,8 +12,9 @@ If (-NOT ($null -eq $updates)) {
     Restart-Computer
 } Else {
     # delete updater.bat from its path
-    Remove-Item 'C:\Users\city\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\update.bat'
+    Remove-Item 'C:\Users\city\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\update.bat' -ErrorAction SilentlyContinue
     # run the next script
+    & '.\dell.ps1'
 
     #stop execution
 }
