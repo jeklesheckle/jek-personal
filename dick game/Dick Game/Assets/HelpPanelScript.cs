40c01fde1b00ed3dd7c8b10f3b5657485ac42682
Jek PC <jackpielstick@gmail.com>
2020-03-25 20:44:20 -0400

    Main menu mockup complete

---
+++ dick game/Dick Game/Assets/HelpPanelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpPanelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // used by HelpButton to make panel appear
+    public void SetActive(bool state)
+    {
+        this.gameObject.SetActive(state);
+    }
+}
