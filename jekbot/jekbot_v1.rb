adc7e20c7d002f07823d35434f5dd6eb50da6bf9
Jack Pielstick <40350094+jeklesheckle@users.noreply.github.com>
2021-12-21 14:09:29 -0500

    remove sensitive info
    token has already been deactivated but I've removed it here anyway for funsies even tho git should have it up basically forever.

--- jekbot/jekbot_v1.rb
+++ jekbot/jekbot_v1.rb
@@ -2,7 +2,7 @@
 
 require 'discordrb'
 
-jekbot = Discordrb::Bot.new token: 'NTQ4MzE3NDQ2MjE4NzExMDYw.D1D83Q.H_p5pKVnr0yDfi-OxBPhcH5zU_8', client_id: 548317446218711060
+jekbot = Discordrb::Bot.new token: ######### redacted api token and personal ID ###########
 
 jekbot.message(with_text: '*they\'re') do |event|
 	event.respond 'INCORRECT USE OF "THEIR" DETECTED'
