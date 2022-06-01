1d6d418acfa973b790049e11809e73546f5ff7f7
Jek PC <jackpielstick@gmail.com>
2022-05-31 22:56:34 -0400

    attempt 1 to print mkv file

--- rust_time/tomfuckery/src/main.rs
+++ rust_time/tomfuckery/src/main.rs
@@ -1,3 +1,19 @@
+/*
+ * Temporary TODO:
+ * * print the whole thing 
+ */
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut mkv_file = File::open("mkv_sample.mkv").expect("Error opening file");
+    let mut mkv_contents = String::new();
+    mkv_file.read_to_string(&mut mkv_contents).expect("Error reading file to string");
+    println!("{}", mkv_contents)
 }
+
+/* thread 'main' panicked at 'Error reading file to string: Error { kind: InvalidData, message: "stream did not contain valid UTF-8" }', src\main.rs:11:48
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+error: process didn't exit successfully: `target\debug\tomfuckery.exe` (exit code: 101) */
+
+// Maybe instead of read_to_string we use some sort of read_to_binary?
